c259dd781a7414f0a553661185279b9aee642069
Luca <87448287+LUCA-PYTHON@users.noreply.github.com>
2026-02-12 16:28:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,34 +8,15 @@ token = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+intents.guilds = True
+intents.presences = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Eingeloggt als {bot.user}")
-
-
-@bot.command(name="sync")
-@commands.is_owner()
-async def sync(ctx, guild: int = None):
-    if guild:
-        guild_obj = discord.Object(id=guild)
-        await bot.tree.sync(guild=guild_obj)
-        await ctx.send(f"Commands für Guild {guild} synchronisiert!")
-    else:
-        await bot.tree.sync()
-        await ctx.send("Globale Commands synchronisiert")
-
-@bot.command(name="reload")
-@commands.is_owner()
-async def reload(ctx, name):
-    try:
-        await bot.reload_extension(f"datein.{name}")
-    except commands.ExtensionNotLoaded:
-        await bot.load_extension(f"datein.{name}")
-    await ctx.send(f"Die Datei {name}.py wurde neu geladen!")
-
 
 async def load_extensions():
     for filename in os.listdir("datein"):
